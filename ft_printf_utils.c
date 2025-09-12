3bbc796424c9af8810b56033f1e3f7fd19e2fe6b
leonardecavele <leonard.decavele@gmail.com>
2025-09-12 03:43:25 +0200

    fix: putnbr was not printing (NULL) when NULL, added puthex

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 21:52:07 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/09/11 22:10:01 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/09/12 03:38:53 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ int	szputchar(char c)
 	return (1);
 }
 
-int	szputstr(char *s)
+int	szputstr(const char *s)
 {
 	int	sz;
 
 	if (!s)
-		return (0);
+		s = "(null)";
 	sz = -1;
 	while (s[++sz])
 		;
@@ -31,23 +31,50 @@ int	szputstr(char *s)
 	return (sz);
 }
 
-int	szputnbr(int n)
+int	szputnbr(long long n)
 {
-	long	nb;
 	int		sz;
 	char	c;
 
-	nb = n;
 	sz = 0;
-	if (nb < 0)
+	if (n < 0)
 	{
 		szputchar('-');
-		nb = -nb;
+		n = -n;
 		sz++;
 	}
-	if (nb > 9)
-		sz += szputnbr(nb / 10);
-	c = nb % 10 + '0';
+	if (n > 9)
+		sz += szputnbr(n / 10);
+	c = n % 10 + '0';
 	szputchar(c);
 	return (sz + 1);
+}
+
+int	szputhex(unsigned long long n, char c, char a)
+{
+	int		sz;
+	char	b[16];
+	int		d;
+	int		i;
+
+	sz = 0;
+	i = 0;
+	if (n == 0 && c == 'p')
+		return (szputstr("(nil)"));
+	if (n == 0)
+		return (szputchar('0'));
+	if (c == 'p')
+		sz += szputstr("0x");
+	while (n)
+	{
+		d = (unsigned int)(n & 0xF);
+		if (d < 10)
+			b[i++] = '0' + d;
+		else
+			b[i++] = a + (d - 10);
+		n >>= 4;
+	}
+	while (i--)
+		sz += szputchar(b[i]);
+	return (sz);
 }
