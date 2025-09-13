5dacafd449dbbf3322be8bf382d33e797f4b0d05
leonardecavele <leonard.decavele@gmail.com>
2025-09-14 00:00:14 +0200

    feat: now '-0.# +' works

---
+++ format.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   format.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 21:03:39 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/09/13 23:46:37 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	prefix(char c, t_format *f)
+{
+	if (ft_strchr("di", c) && f->v < 0)
+		szputchar('-', 1);
+	else if (ft_strchr("di", c) && f->flags & FPL)
+		szputchar('+', 1);
+	else if (ft_strchr("di", c) && f->flags & FSP)
+		szputchar(' ', 1);
+	if (c == 'x' && f->flags & FHS && f->v)
+		szputnstr("0x", 1, 2);
+	else if (c == 'X' && f->flags & FHS && f->v)
+		szputnstr("0X", 1, 2);
+}
+
+static int	zer(t_format *f)
+{
+	int	n;
+
+	n = 0;
+	if (!(f->flags & FMN) && f->flags & FZR && f->pre < 0)
+		while (f->pad--)
+			n += szputchar('0', 1);
+	else
+		while (f->zer--)
+			n += szputchar('0', 1);
+	return (n);
+}
+
+static int	lpad(t_format *f)
+{
+	int	n;
+
+	n = 0;
+	if (!(f->flags & FMN) && (f->pre > -1 || !(f->flags & FZR)))
+		while (f->pad--)
+			n += szputchar(' ', 1);
+	return (n);
+}
+
+static int	rpad(t_format *f)
+{
+	int	n;
+
+	n = 0;
+	if (f->flags & FMN)
+		while (f->pad--)
+			n += szputchar(' ', 1);
+	return (n);
+}
+
+int	format(char c, va_list pm, t_format *f)
+{
+	int		n;
+
+	n = 0;
+	if (c == 'p' && !f->v)
+		return (szputnstr("(nil)", 1, 5));
+	if (ft_strchr("di", c) && (f->flags & FPL || f->flags & FSP || f->v < 0))
+		n += 1;
+	if (ft_strchr("xX", c) && (f->flags & FHS) && f->v)
+		n += 2;
+	if (c == 'p' && f->v)
+		n += szputnstr("0x", 1, 2);
+	if (f->pre > -1 && ft_strchr("diuxX", c))
+		f->zer = max(0, f->pre - f->n);
+	f->pad = max(0, f->wid - (n + f->zer + f->n));
+	n += lpad(f);
+	prefix(c, f);
+	n += zer(f);
+	if (!(ft_strchr("diuxX", c) && f->pre == 0 && f->v == 0))
+		n += szputpm(c, pm, 1, f->n);
+	n += rpad(f);
+	return (n);
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,162 +6,103 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 18:38:50 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/09/13 01:02:37 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/09/13 23:46:03 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <unistd.h>
 
-t_ll	get_value(char c, va_list pm)
+static t_ll	value(char c, va_list pm)
 {
-	va_list cp;
 	t_ll	v;
 
-	va_copy(cp, pm);
 	v = 0;
-	if (c == 'c' || c == 'd' || c == 'i')
-		v = (t_ll)va_arg(cp, int);
+	if (ft_strchr("cdi", c))
+		v = (t_ll)va_arg(pm, int);
 	else if (c == 's')
-		v = (t_ll)va_arg(cp, const char *);
+		v = (t_ll)va_arg(pm, const char *);
 	else if (c == 'p')
-		v = (t_ll)va_arg(cp, void *);
-	else if (c == 'u' || c == 'x' || c == 'X')
-		v = (t_ll)va_arg(cp, unsigned int);
-	va_end(cp);
+		v = (t_ll)va_arg(pm, void *);
+	else if (ft_strchr("uxX", c))
+		v = (t_ll)va_arg(pm, unsigned int);
 	return (v);
 }
 
-static int	handle_flag(char c, va_list pm, t_flags *f)
-{
-	t_ll	v;
-	int		n;
-
-	n = 0;
-	if (c == 'c' || c == 's' || c == '%')
-		f->f &= ~(Z_F | P_F | S_F | H_F);
-	v = get_value(c, pm);
-	if ((c == 'd' || c == 'i' || c == 'u' || c == 'x' || c == 'X') && !f->p && !v)
-		f->n = 0;
-	if (c == 's' && f->p > -1)
-		f->n = f->p;
-	if (c == 'p' && !v)
-		return (write(1, "(nil)", 5));
-	if ((c == 'd' || c == 'i') && (f->f & P_F || f->f & S_F || v < 0))
-		n += 1;
-	if ((c == 'x' || c == 'X') && (f->f & H_F) && v)
-		n += 2;
-	if (c == 'p' && v && !(f->w > 0 && !(f->f & M_F) && !(f->f & Z_F)))
-		n += szputnstr("0x", 1, 2);
-	else if (c == 'p' && v)
-		n += 2;
-	if (f->p > -1)
-		f->z = max(0, f->p - f->n); 
-	f->d = max(0, f->w - (n + f->z + f->n));
-	if (!(f->f & M_F) && (f->p > -1 || !(f->f & Z_F)))
-		while (f->d--)
-			n += szputchar(' ', 1);
-	if (c == 'p' && v && f->w > 0 && !(f->f & M_F) && !(f->f & Z_F))
-		n += szputnstr("0x", 1, 2);
-	if ((c == 'd' || c == 'i') && v < 0)
-		szputchar('-', 1);
-	else if ((c == 'd' || c == 'i') && f->f & P_F)
-		szputchar('+', 1);
-	else if ((c == 'd' || c == 'i') && f->f & S_F)
-		szputchar(' ', 1);
-	if ((c == 'x') && f->f & H_F && v)
-		write(1, "0x", 2);
-	else if ((c == 'X') && f->f & H_F && v)
-		write(1, "0X", 2);
-	if (!(f->f & M_F) && f->f & Z_F && f->p < 0)
-		while (f->d-- && c != 'c')
-			n += szputchar('0', 1);
-	else
-		while (f->z-- && c != 'c')
-			n += szputchar('0', 1);
-	if (!(!f->p && !v))
-		n += szputpm(c, pm, 1, f->n);
-	if (f->f & M_F)
-		while (f->d--)
-			n += szputchar(' ', 1);
-	return (n);
-}
-
-static short	get_flag(char **s)
+static short	flags(char **s)
 {
 	short	f;
 
 	f = 0;
-	while (**s && (**s == '-' || **s == '0' || **s == '#'
-			|| **s == '+' || **s == ' '))
+	while (**s && ft_strchr("-0#+ ", **s))
 	{
 		if (**s == '-')
-			f |= M_F;
+			f |= FMN;
 		else if (**s == '0')
-			f |= Z_F;
+			f |= FZR;
 		else if (**s == '#')
-			f |= H_F;
+			f |= FHS;
 		else if (**s == '+')
-			f |= P_F;
+			f |= FPL;
 		else if (**s == ' ')
-			f |= S_F;
+			f |= FSP;
 		(*s)++;
 	}
-	if (f & M_F)
-		f &= ~Z_F;
-	if (f & P_F)
-		f &= ~S_F;
+	if (f & FMN)
+		f &= ~FZR;
+	if (f & FPL)
+		f &= ~FSP;
 	return (f);
 }
 
-static int	parse(char **s, va_list pm, t_flags *f)
+static int	parse(char **s, va_list pm, t_format *f)
 {
+	va_list	tmp;
 	int		n;
 
 	n = 0;
-	f->f = get_flag(s);
 	if (**s && **s >= '0' && **s <= '9')
-		f->w = sm_atoii(s);
+		f->wid = sm_atoii(s);
 	if (**s && **s == '.')
 	{
 		(*s)++;
-		f->p = 0;
+		f->pre = 0;
 		if (**s && **s >= '0' && **s <= '9')
-			f->p = sm_atoii(s);
+			f->pre = sm_atoii(s);
 	}
-	if (**s == 'c' || **s == 's' || **s == 'p' || **s == 'd' || **s == 'i'
-		|| **s == 'u' || **s == 'x' || **s == 'X' || **s == '%')
+	if (ft_strchr("cs%", **s))
+		f->flags &= ~(FZR | FPL | FSP | FHS);
+	if (ft_strchr("cspdiuxX%", **s))
 	{
-		f->n = szputpm(**s, pm, -1, 2147483647);
-		n += handle_flag(**s, pm, f);
+		va_copy(tmp, pm);
+		f->v = value(**s, pm);
+		f->n = szputpm(**s, tmp, -1, 2147483647);
+		if (**s == 's' && f->pre > -1)
+			f->n = min(f->n, f->pre);
+		va_end(tmp);
 	}
-	else
-		return (-1);
-	return (n);
+	return (n + format(**s, tmp, f));
 }
 
 int	ft_printf(const char *s, ...)
 {
-	va_list	pm;
-	t_flags	f;
-	int		n;
+	va_list		pm;
+	t_format	f;
+	int			n;
 
 	n = 0;
 	va_start(pm, s);
 	while (*s && n > -1)
 	{
-		f.f = 0;
-		f.w = 0;
-		f.p = -1;
-		f.z = 0;
-		f.n = 0;
+		f = (t_format){0};
+		f.pre = -1;
 		if (*s == '%')
 		{
 			s++;
-			n += parse((char **)&s, pm, &f); //problem here
+			f.flags = flags((char **)&s);
+			n += parse((char **)&s, pm, &f);
 		}
 		else
-			n += write(1, s, 1);
+			n += szputchar(*s, 1);
 		if (*s)
 			s++;
 	}
