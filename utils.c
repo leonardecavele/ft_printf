db26fab98abb1b71297dacf632078ed16b5cd496
leonardecavele <leonard.decavele@gmail.com>
2025-09-14 00:01:27 +0200

    added 'min' and 'ft_strchr'

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 16:03:57 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/09/13 00:32:26 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/09/13 21:46:07 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,29 @@ int	sm_atoii(char **nptr)
 	return (result);
 }
 
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if ((t_u8)(*s) == (t_u8)c)
+			return ((char *)s);
+		s++;
+	}
+	if (!(t_u8)c)
+		return ((char *)s);
+	return (0);
+}
+
 int	max(int a, int b)
 {
 	if (a > b)
+		return (a);
+	return (b);
+}
+
+int	min(int a, int b)
+{
+	if (a < b)
 		return (a);
 	return (b);
 }
