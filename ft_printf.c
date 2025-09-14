76e001c08c352ed01a6d16922488f21052a6e7e6
leonardecavele <leonard.decavele@gmail.com>
2025-09-14 15:02:11 +0200

    changed prototypes and moved 'szputpm' to 'szput.c'

--- ft_printf.c
+++ ft_printf.c
@@ -6,37 +6,11 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 18:38:50 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/09/14 00:03:47 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/09/14 15:00:52 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdarg.h>
-#include <unistd.h>
-
-static int	handle_arg(const char *s, va_list pm)
-{
-	int	n;
-
-	n = 0;
-	if (*s == 'c')
-		n += szputchar(va_arg(pm, int));
-	else if (*s == 's')
-		n += szputstr(va_arg(pm, const char *));
-	else if (*s == 'p')
-		n += szputhex((unsigned long long)va_arg(pm, void *), *s, 'a');
-	else if (*s == 'd' || *s == 'i')
-		n += szputnbr((int)va_arg(pm, int));
-	else if (*s == 'u')
-		n += szputnbr((unsigned int)va_arg(pm, unsigned int));
-	else if (*s == 'x')
-		n += szputhex((unsigned long long)va_arg(pm, unsigned int), *s, 'a');
-	else if (*s == 'X')
-		n += szputhex((unsigned long long)va_arg(pm, unsigned int), *s, 'A');
-	else if (*s == '%')
-		n += write(1, "%", 1);
-	return (n);
-}
 
 int	ft_printf(const char *s, ...)
 {
@@ -50,7 +24,7 @@ int	ft_printf(const char *s, ...)
 		if (*s == '%')
 		{
 			s++;
-			n += handle_arg(s, pm);
+			n += szputpm(pm, *s);
 		}
 		else
 			n += write(1, s, 1);
--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,18 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 21:09:29 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/09/12 11:14:17 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/09/14 15:00:42 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *s, ...);
+# include <stdarg.h>
+# include <unistd.h>
 
-//utils
-int	szputchar(char c);
-int	szputstr(const char *s);
-int	szputnbr(long long n);
-int	szputhex(unsigned long long n, char c, char a);
+int	ft_printf(const char *s, ...);
+//szput
+int	szputpm(va_list pm, char c);
 
 #endif
