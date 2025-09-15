009b4c3211c277343babdcce0bb1f5e284f56733
leonardecavele <leonard.decavele@gmail.com>
2025-09-15 17:02:45 +0200

    fix: now works with precision between 0 and 6 and width > 0 on strings

--- bonus/format_bonus.c
+++ bonus/format_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 21:03:39 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/09/15 10:42:56 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/09/15 16:46:13 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,23 +40,29 @@ static int	zer(t_format *f)
 	return (n);
 }
 
-static int	lpad(t_format *f)
+static int	lpad(char c, t_format *f)
 {
 	int	n;
 
 	n = 0;
-	if (!(f->flags & FMN) && (f->pre > -1 || !(f->flags & FZR)))
+	if (f->pre > -1 && f->pre < 6 && c == 's' && f->v == 0)
+	{
+		while (f->wid--)
+			n += write(1, " ", 1);
+		f->pad = 0;
+	}
+	if (!(f->flags & FMN) && (f->pre > -1 || !(f->flags & FZR)) && c != '%')
 		while (f->pad--)
 			n += write(1, " ", 1);
 	return (n);
 }
 
-static int	rpad(t_format *f)
+static int	rpad(char c, t_format *f)
 {
 	int	n;
 
 	n = 0;
-	if (f->flags & FMN)
+	if (f->flags & FMN && c != '%')
 		while (f->pad--)
 			n += write(1, " ", 1);
 	return (n);
@@ -71,8 +77,6 @@ int	format(char c, va_list pm, t_format *f)
 		f->n = 0;
 	if (c == 's' && f->pre > -1)
 		f->n = min(f->n, f->pre);
-	if (c == 's' && f->pre > -1 && f->pre < 6 && f->v == 0)
-		return (0);
 	if (ft_strchr("di", c) && (f->flags & FPL || f->flags & FSP || f->v < 0))
 		n += 1;
 	if (ft_strchr("xX", c) && (f->flags & FHS) && f->v)
@@ -82,11 +86,13 @@ int	format(char c, va_list pm, t_format *f)
 	if (f->pre > -1 && ft_strchr("diuxX", c))
 		f->zer = max(0, f->pre - f->n);
 	f->pad = max(0, f->wid - (n + f->zer + f->n));
-	n += lpad(f);
+	n += lpad(c, f);
+	if (c == 's' && f->pre > -1 && f->pre < 6 && f->v == 0)
+		return (n);
 	prefix(c, f);
 	n += zer(f);
 	if (!(ft_strchr("diuxX", c) && f->pre == 0 && f->v == 0))
 		n += szputpm_fd(1, pm, c, f->n);
-	n += rpad(f);
+	n += rpad(c, f);
 	return (n);
 }
