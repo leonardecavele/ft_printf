c4e0f0a400f4e72dbbab3a188f99dc40b5884283
Leonard Decavele <ldecavel@z2r3p5.42lyon.fr>
2025-11-06 12:32:48 +0100

    fix: uses 'attribute' to prevent error at compilation

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 21:09:29 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/09/14 15:18:44 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/05 13:53:08 by ldecavel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(const char *s, ...);
+int	ft_printf(const char *s, ...) __attribute__((format(printf, 1, 2)));
 
 #endif
